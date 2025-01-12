4321d68d57003c06dd4cb1e28c979675e79541fb
Tiago Buas <Tiagobuas.tiagobuas@gmail.com>
2025-01-11 22:54:37 -0300

    Add wait_for_db.js

---
+++ infra/scripts/wait_for_db.js
@@ -0,0 +1,16 @@
+/** nothing for now **/
+const { exec } = require('node:child_process');
+
+
+function checkDB(){
+	exec('docker exec postgresDev pg_isready --host localhost', (err, stdout) => {
+		if (stdout.search('accepting connections') === -1){
+			process.stdout.write('.');
+			checkDB();
+			return;
+		}
+		console.log('\n🟢 db initialized');	
+	});
+}
+process.stdout.write('🔴 initiaziling db');
+checkDB();
