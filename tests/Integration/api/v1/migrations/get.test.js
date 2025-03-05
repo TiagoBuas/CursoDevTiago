6cd3f8151d4b8df4c19e2c561384f505cd534462
TiagoBuas <tiagobuas@gmail.com>
2025-03-04 21:29:36 -0300

    refactor: refactors tests, eslint and encapsulates the npm run dev

--- tests/Integration/api/v1/migrations/get.test.js
+++ tests/Integration/api/v1/migrations/get.test.js
@@ -1,17 +1,19 @@
 //Teste da ferramenta get do /api/v1/migrations
-
-import database from "infra/database.js";
 import orchestrator from "tests/orchestrator.js";
 
 beforeAll(async () => {
-  await database.clearDatabase();
+  await orchestrator.clearDatabase();
   await orchestrator.wait_for_all_services();
 });
 
-test("responde should be 200 from the request status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+describe("Anonymous user", () => {
+  describe("get to api/v1/migrations", () => {
+    test("retrieve pendings migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
+      const responseBody = await response.json();
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
